ee458fccac93094ee5ab433ae631586cce1c0151
Christian Mötzing <christian.motzing@quantum-machines.co>
2024-05-03 09:14:08 +0200

    Upgrade to aws-sdk v3

--- src/aws.js
+++ src/aws.js
@@ -1,6 +1,11 @@
-const AWS = require('aws-sdk');
+const { EC2Client, RunInstancesCommand, TerminateInstancesCommand, waitUntilInstanceRunning } = require("@aws-sdk/client-ec2");
 const core = require('@actions/core');
 const config = require('./config');
+const buffer = require('buffer');
+const ec2Client = new EC2Client({
+  region: config.input.region,
+  maxAttempts: 5,
+});
 
 // User data scripts are run as the root user
 function buildUserDataScript(githubRegistrationToken, label) {
@@ -33,25 +38,37 @@ function buildUserDataScript(githubRegistrationToken, label) {
 }
 
 async function startEc2Instance(label, githubRegistrationToken) {
-  const ec2 = new AWS.EC2();
-
   const userData = buildUserDataScript(githubRegistrationToken, label);
-
-  const params = {
+  const runInstancesCommandInput = {
     ImageId: config.input.ec2ImageId,
     InstanceType: config.input.ec2InstanceType,
-    MinCount: 1,
     MaxCount: 1,
-    UserData: Buffer.from(userData.join('\n')).toString('base64'),
+    MinCount: 1,
+    Monitoring: {
+      Enabled: true,
+    },
+    SecurityGroupIds: [
+      config.input.securityGroupId,
+    ],
     SubnetId: config.input.subnetId,
-    SecurityGroupIds: [config.input.securityGroupId],
-    IamInstanceProfile: { Name: config.input.iamRoleName },
+    UserData: buffer.from(userData.join('\n')).toString('base64'),
+    DisableApiTermination: false,
+    DryRun: false,
+    EbsOptimized: true,
+    IamInstanceProfile: {
+      Name: config.input.iamRoleName,
+    },
+    InstanceInitiatedShutdownBehavior: "terminate",
     TagSpecifications: config.tagSpecifications,
+    MaintenanceOptions: {
+      AutoRecovery: "disabled",
+    },
   };
+  const runInstancesCommand = new RunInstancesCommand(runInstancesCommandInput);
 
   try {
-    const result = await ec2.runInstances(params).promise();
-    const ec2InstanceId = result.Instances[0].InstanceId;
+    const response = await ec2Client.send(runInstancesCommand);
+    const ec2InstanceId = response.Instances[0].InstanceId;
     core.info(`AWS EC2 instance ${ec2InstanceId} is started`);
     return ec2InstanceId;
   } catch (error) {
@@ -61,14 +78,19 @@ async function startEc2Instance(label, githubRegistrationToken) {
 }
 
 async function terminateEc2Instance() {
-  const ec2 = new AWS.EC2();
-
-  const params = {
-    InstanceIds: [config.input.ec2InstanceId],
+  const terminateInstancesCommandInput = {
+    InstanceIds: [
+      config.input.ec2InstanceId,
+    ],
+    DryRun: false,
   };
+  const terminateInstancesCommand = new TerminateInstancesCommand(terminateInstancesCommandInput);
 
   try {
-    await ec2.terminateInstances(params).promise();
+    const response = await ec2Client.send(terminateInstancesCommand);
+    const currentState = response.TerminatingInstances[0].CurrentState.Name;
+    const previousState = response.TerminatingInstances[0].PreviousState.Name;
+    core.debug(`AWS EC2 instance ${config.input.ec2InstanceId} transitioned from ${previousState} to ${currentState}`);
     core.info(`AWS EC2 instance ${config.input.ec2InstanceId} is terminated`);
     return;
   } catch (error) {
@@ -78,14 +100,8 @@ async function terminateEc2Instance() {
 }
 
 async function waitForInstanceRunning(ec2InstanceId) {
-  const ec2 = new AWS.EC2();
-
-  const params = {
-    InstanceIds: [ec2InstanceId],
-  };
-
   try {
-    await ec2.waitFor('instanceRunning', params).promise();
+    await waitUntilInstanceRunning({ client: ec2Client, maxWaitTime: 300, minDelay: 10 }, { InstanceIds: [ ec2InstanceId ] })
     core.info(`AWS EC2 instance ${ec2InstanceId} is up and running`);
     return;
   } catch (error) {
